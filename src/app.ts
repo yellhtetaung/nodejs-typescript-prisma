2c4ad1271a7a29fbf15d0d2625feddbd8cf79205
yellhtetaung <yehtetaung.dev@gmail.com>
2023-11-19 01:46:57 +0630

    Deploying: user route

--- src/app.ts
+++ src/app.ts
@@ -7,26 +7,25 @@ import cors from "cors";
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 8001;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors());
 
-app.get("/", (req: express.Request, res: express.Response) => {
-	res.status(200).send(`
-    <!DOCTYPE html>
-    <html>
-      <head>
-        <title>Node & TypeScript</title>
-      </head>
-      <body>
-        <h1>Node & TypeScript🚀</h1>
-      </body>
-    </html>
-  `);
-});
+// app.get("/", (req: express.Request, res: express.Response) => {
+// 	res.status(200).send(`
+//     <!DOCTYPE html>
+//     <html>
+//       <head>
+//         <title>Node & TypeScript</title>
+//       </head>
+//       <body>
+//         <h1>Node & TypeScript🚀</h1>
+//       </body>
+//     </html>
+//   `);
+// });
 
 app.use("/api", require("./users/users.route").default);
 
